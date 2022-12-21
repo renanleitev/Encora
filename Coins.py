28ebb99afb67ea11830f2e2f210a31dc480ca30f
Renan Leite Vieira <109419403+renanleitev@users.noreply.github.com>
2022-12-21 11:17:34 -0300

    Update Coins.py

--- Coins.py
+++ Coins.py
@@ -5,7 +5,7 @@ def makeChange(amount):
   # Showing the value of each coin (quarters, dimes, nickels and pennies)
   coins = [25, 10, 5, 1]
   # Final set (to show all possible results)
-  final = []
+  final = set()
   # Loop through the coins to show all possible coins (nested loop), to the most value (quarter) to lowest value (pennies)
   # Each "for" represents a type of coin and the maximum value that can be reached, according to the "amount" variable
   # P.S: +1 = to compensate the final range
@@ -15,12 +15,12 @@ def makeChange(amount):
         # Pennies don't need to loop "for" (it is the lowest value == 1)
         pennies=amount-coins[0]*quarters-coins[1]*dimes-coins[2]*nickels
         # Temporarily stores the result of the loop
-        result = [quarters, dimes, nickels, pennies]
+        result = (quarters, dimes, nickels, pennies)
         # Append the temporary result into the final set
-        final.append(result)
+        final.add(result)
   # Returns the set
   return final
 # Output the set on terminal
 print(makeChange(12))
-
-
+# Output the type == set
+print(type(makeChange(12)))
